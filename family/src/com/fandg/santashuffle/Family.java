8b1dff7169edbd7a343645d15c8c64d50e156507
Arden Gudger <arden@wolf-tek.com>
2013-11-27 16:16:06 -0800

    splash screen and images fixed choose

--- family/src/com/fandg/santashuffle/Family.java
+++ family/src/com/fandg/santashuffle/Family.java
@@ -1,7 +1,12 @@
 package com.fandg.santashuffle;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
 import android.widget.ListView;
 
 
@@ -9,6 +14,14 @@ public class Family extends Application{
 	
 	private static ArrayList<String> names = new ArrayList<String>();
 	private static ArrayList<String> recipients = new ArrayList<String>();
+	
+	public static ArrayList<Integer> chosenIndicies = new ArrayList<Integer>();
+	public static String _APP_VERSION = "";
+	public static ArrayList<Integer> getChosenIndicies() {
+		return chosenIndicies;
+	}
+
+
 
 	public static int getCurrentSelection() {
 		return _currentSelection;
@@ -48,6 +61,21 @@ public class Family extends Application{
 	}
 	
 
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		Context context = getApplicationContext(); // or activity.getApplicationContext()
+		PackageManager packageManager = context.getPackageManager();
+		String packageName = context.getPackageName();
 
+	
+		// Obtains the app version based in the manifest file.
+		try {
+		    _APP_VERSION = packageManager.getPackageInfo(packageName, 0).versionName;
+		} catch (PackageManager.NameNotFoundException e) {
+
+		}
+	}
 
 }
