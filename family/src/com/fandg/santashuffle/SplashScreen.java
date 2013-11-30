fd2807bca43b39696ecc0904d400d5b117d52e86
dan <dan.forrest@gmail.com>
2013-11-30 15:48:50 -0800

    Added tap to close on the splash screen.  Hide bottom two rows when skb is active so smaller devices can see the name list.

--- family/src/com/fandg/santashuffle/SplashScreen.java
+++ family/src/com/fandg/santashuffle/SplashScreen.java
@@ -1,22 +1,12 @@
 package com.fandg.santashuffle;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.TextView;
-
 
 /**
  * 
@@ -25,61 +15,83 @@ import android.widget.TextView;
  * This activity has not history - after it it finishes it is destroyed and will
  * only be created again after a crash or initial start
  * 
- * For this class to work you will need the supporting layout.xml or create your own.
+ * For this class to work you will need the supporting layout.xml or create your
+ * own.
  * 
  * @author Arden
  * 
  */
 public class SplashScreen extends Activity {
-	protected int _splashTime = 5000;
-
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		
-		setContentView(R.layout.splashscreen);
-		
-		 Thread background = new Thread() {
-	            public void run() {
-	                 
-	                try {
-	                    // Thread will sleep for 5 seconds
-	                    sleep(_splashTime);
-	                     
-	                    // After 5 seconds redirect to another intent
-	                    Intent i=new Intent(getBaseContext(),Main.class);
-	                    startActivity(i);
-	                     
-	                    //Remove activity
-	                    finish();
-	                     
-	                } catch (Exception e) {
-	                 
-	                }
-	            }
-	        };
-	         
-	        // start thread
-	        background.start();
-	}
-	
-	@Override
-	public void finish() {
-		super.finish();
-	}
-
-	@Override
-	public void onBackPressed() {
-		this.finish();
-		super.onBackPressed();
-	}
-	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		TextView version = (TextView) findViewById(R.id.versionnumber);
-		version.setText(Family._APP_VERSION);
-	}
+    protected int     _splashTime  = 5000;
+    protected boolean screenTapped = false;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.splashscreen);
+        
+        final Thread background = new Thread() {
+            public void run() {
+                
+                try {
+                    
+                    screenTapped = false;
+                    // Thread will sleep for 5 seconds
+                    for (int i = 0; i < 100; i++) {
+                        if (screenTapped == true) {
+                            break;
+                        }
+                        sleep(_splashTime / 100);
+                    }
+                    
+                    // After 5 seconds redirect to another intent
+                    Intent i = new Intent(getBaseContext(), Main.class);
+                    startActivity(i);
+                    
+                    // Remove activity
+                    finish();
+                    
+                } catch (Exception e) {
+                    
+                }
+            }
+        };
+        
+        // start thread
+        background.start();
+        
+        // Close the splash screen by tapping the screen
+        View splashView = findViewById(R.id.splashscreen);
+        splashView.setOnTouchListener(new OnTouchListener() {
+            
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                screenTapped = true;
+                
+                return true;
+            }
+            
+        });
+        
+    }
+    
+    @Override
+    public void finish() {
+        super.finish();
+    }
+    
+    @Override
+    public void onBackPressed() {
+        this.finish();
+        super.onBackPressed();
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView version = (TextView) findViewById(R.id.versionnumber);
+        version.setText(Family._APP_VERSION);
+        
+    }
 }
